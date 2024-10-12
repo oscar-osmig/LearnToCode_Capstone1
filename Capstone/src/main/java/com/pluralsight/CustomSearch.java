88916f831d21ab4546de3f4451677aaa09684ed2
Osmig_Torres <torresosmig@gmail.com>
2024-10-12 00:07:47 -0700

    Started challenge custom search

---
+++ Capstone/src/main/java/com/pluralsight/CustomSearch.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CustomSearch {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
