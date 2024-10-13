38baddf5398c52903c954cf2460a849ed561c3f2
Osmig_Torres <torresosmig@gmail.com>
2024-10-12 19:08:15 -0700

    texted comparing dates

--- Capstone/src/main/java/com/pluralsight/CustomSearch.java
+++ Capstone/src/main/java/com/pluralsight/CustomSearch.java
@@ -1,6 +1,9 @@
 package com.pluralsight;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,6 +11,8 @@ public class CustomSearch {
     static Scanner scanner = new Scanner(System.in);
 
     public static void customSearch(Scanner scanner, ArrayList<Transaction> transactions){
+        ArrayList<Transaction> transactionDate = transactions;
+
         System.out.println("* Press <enter> to live category blank *");
         // start date
         System.out.println("Start date: (yyyy-mm-dd)");
@@ -25,6 +30,15 @@ public class CustomSearch {
         System.out.println("Amount:");
         int amount = scanner.nextInt();
         // if not enter value variable not needed in the search
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); //formatting the input date
+        LocalDate theStartDate = LocalDate.parse(startDate, formatter);
+        //LocalDate theEndDate =  LocalDate.parse(endDate, formatter);
+        // check is input date is after start day
+
+        for(Transaction dateToCompare : transactionDate){
+            boolean isBeforeStartDate = theStartDate.isBefore(dateToCompare.getDate());
+            System.out.println(isBeforeStartDate);
+        }
 
 
     }
